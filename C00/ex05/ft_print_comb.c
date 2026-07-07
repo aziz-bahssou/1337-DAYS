7be4e22fdce303e74c221a1168852cdadbf39e01
Aziz <azizbahssou444@gmail.com>
2026-07-07 16:38:42 +0100

    print comb

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+void ft_putchar(char c){
+    write(1,&c,1);
+}
+
+void ft_print_comb(void){
+
+    int i = 0;
+    int j;
+    int k;
+
+    while (i < 8){
+        j = i + 1;
+        while (j < 9){
+            k = j + 1;
+            while (k <= 9){
+                ft_putchar(i + '0');
+                ft_putchar(j + '0');
+                ft_putchar(k + '0');
+                if(!(i == 7 && j == 8 && k == 9)){
+                    write(1, ", ",2);
+                }
+                k++;
+            }           
+        j++;    
+        }
+        i++;
+    }    
+}
