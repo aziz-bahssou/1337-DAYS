60d0bbfcf79c3d53a1a706b1f4215dd03abb1a7c
Aziz <azizbahssou444@gmail.com>
2026-07-07 13:56:09 +0100

    print_alphabet

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+void ft_print_alphabet(){
+    char c = 'a';
+    while (c <= 'z'){
+        write(1,&c,1);
+        c++;
+    }
+    
+}
+int main(){
+    ft_print_alphabet();
+}
